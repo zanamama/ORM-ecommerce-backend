a8c1252ad100dc0211beb24fa1c9abc9898a5d76
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-21 00:19:00 -0400

    tag-routes updated

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,6 +4,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  console.log("Lookin' Gooood!");
   // find all tags
   // be sure to include its associated Product data
 });
@@ -23,6 +24,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagRoutesData = await tag-routes.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagRoutesData) {
+      res.status(404).json({ message: 'No routes were found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagRoutesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 });
 
 module.exports = router;

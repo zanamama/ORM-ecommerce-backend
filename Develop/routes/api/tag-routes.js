8eecd4a9f429d1b0e3a083602bcd6761716f0829
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-21 11:11:42 -0400

    updated tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,46 +1,40 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   console.log("Lookin' Gooood!");
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
-  try {
-    const tagRoutesData = await tag-routes.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!tagRoutesData) {
-      res.status(404).json({ message: 'No routes were found with that id!' });
-      return;
-    }
-
-    res.status(200).json(tagRoutesData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then((tagRoutesData) => {
+      if (!tagRoutesData) {
+        res.status(404).json({ message: "Not working!" });
+        return;
+      }
+      res.json(tagRoutesData);
+    })
+    .catch((err) => res.status(500).json(err));
 });
-// });
 
 module.exports = router;
